73843c9ca7e5eb60d8ae9d45d942a3a7014000b8
R3fuSe <1956197657@qq.com>
2025-03-06 10:14:02 +0800

    后端

---
+++ src/test/java/org/example/music_backend/MusicBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.music_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
